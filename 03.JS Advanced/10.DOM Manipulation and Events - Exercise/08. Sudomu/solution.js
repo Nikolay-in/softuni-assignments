8b5b1bcd9e4c14d609684b187425d9c80e684270
niki-369 <nindzhiev@gmail.com>
2022-06-03 10:24:08 +0300

    commit

--- 03.JS Advanced/10.DOM Manipulation and Events - Exercise/08. Sudomu/solution.js
+++ 03.JS Advanced/10.DOM Manipulation and Events - Exercise/08. Sudomu/solution.js
@@ -1,4 +1,49 @@
 function solve() {
+    let [checkBtn, clearBtn] = document.querySelectorAll('button');
+    let table = document.querySelector('div#exercise > table');
+    let result = document.querySelector('div#check > p');
+    checkBtn.addEventListener('click', check);
+    clearBtn.addEventListener('click', clear);
 
-    //TODO...
+    function check() {
+        let matrix = [];
+        let solved = true;
+
+        let rows = Array.from(document.querySelectorAll('table tbody tr'));
+        rows.forEach(row => {
+            let cols = Array.from(row.querySelectorAll('input'));
+            let rowValues = [];
+            cols.forEach(col => rowValues.push(col.value));
+            matrix.push(rowValues);
+        });
+
+        for (let i = 0; i < matrix.length; i++) {
+            let row = new Set();
+            let col = new Set();
+            for (let j = 0; j < matrix[i].length; j++) {
+                row.add(Number(matrix[i][j]));
+                col.add(Number(matrix[j][i]));
+            }
+            if (row.size < 3 || col.size < 3) {
+                solved = false;
+            }
+        }
+
+        if (solved) {
+            table.style.border = '2px solid green';
+            result.style.color = 'green';
+            result.textContent = 'You solve it! Congratulations!';
+        } else {
+            table.style.border = '2px solid red';
+            result.style.color = 'red';
+            result.textContent = 'NOP! You are not done yet...';
+        }
+    }
+    
+    function clear() {
+        let inputs = Array.from(document.querySelectorAll('input[type="number"]'));
+        inputs.forEach(e => e.value = '');
+        table.style.border = 'none';
+        result.textContent = '';
+    }
 }
