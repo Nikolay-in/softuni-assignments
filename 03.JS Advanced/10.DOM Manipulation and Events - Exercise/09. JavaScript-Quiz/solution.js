83330bd1b73cfcfacfff7a343b87d1726ee732cc
niki-369 <nindzhiev@gmail.com>
2022-06-03 15:43:19 +0300

    commit

--- 03.JS Advanced/10.DOM Manipulation and Events - Exercise/09. JavaScript-Quiz/solution.js
+++ 03.JS Advanced/10.DOM Manipulation and Events - Exercise/09. JavaScript-Quiz/solution.js
@@ -1,3 +1,22 @@
 function solve() {
-  //TODO...
+  let questions = Array.from(document.querySelectorAll('section'));
+  let result = document.querySelector('#results h1');
+  let trueAnswers = ['onclick', 'JSON.stringify()', 'A programming API for HTML and XML documents'];
+  let points = 0;
+  function submitAnswer(e) {
+    let answerText = e.currentTarget.querySelector('p').textContent;
+    if (trueAnswers.indexOf(answerText) != -1) {
+      points++;
+    }
+    e.currentTarget.parentElement.parentElement.style.display = 'none';
+    e.currentTarget.parentElement.parentElement.nextElementSibling.style.display = 'block';
+
+    let resultString = (points < 3) ? `You have ${points} right answers` : `You are recognized as top JavaScript fan!`;
+    result.textContent = resultString;
+  }
+
+  for (let i = 0; i < questions.length; i++) {
+    let answers = Array.from(questions[i].querySelectorAll('li[class*="quiz-answer"]'));
+    answers.forEach(e => { e.addEventListener('click', submitAnswer) });
+  }
 }
