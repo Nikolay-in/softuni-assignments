022bf45f2f0f5a22858a500f54264cc485636853
niki-369 <nindzhiev@gmail.com>
2022-06-08 06:28:41 +0300

    commit

--- 03.JS Advanced/12.Advanced Functions - Exercise/05.Functional Sum.js
+++ 03.JS Advanced/12.Advanced Functions - Exercise/05.Functional Sum.js
@@ -10,3 +10,14 @@ function add(num) {
 console.log(add(1)(6)(-3).toString());
 
 //currying
+
+// function add(num) {
+//     if (this.sum !== undefined) {
+//       this.sum += num
+//     } else {
+//       this.sum = num
+//     }
+//     let bindedAdd = add.bind(this)
+//     bindedAdd.toString = () => this.sum
+//     return bindedAdd;
+//   }
