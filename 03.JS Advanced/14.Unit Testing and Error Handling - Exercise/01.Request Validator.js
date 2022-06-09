611f3c29b3d4643e9fc80a92fb06730d6cceb8ae
niki-369 <nindzhiev@gmail.com>
2022-06-09 18:42:50 +0300

    commit

--- 03.JS Advanced/14.Unit Testing and Error Handling - Exercise/01.Request Validator.js
+++ 03.JS Advanced/14.Unit Testing and Error Handling - Exercise/01.Request Validator.js
@@ -2,7 +2,7 @@ function requestValidator(input) {
     const methods = ['GET', 'POST', 'DELETE', 'CONNECT'];
     let uriRegex = new RegExp(/^[\w\d.]+$|^\*$/);
     const versions = ['HTTP/0.9', 'HTTP/1.0', 'HTTP/1.1', 'HTTP/2.0'];
-    let msgRegex = new RegExp(/^[^<>\\&'"]+$/);
+    let msgRegex = new RegExp(/^[^<>\\&'"]*$/);
 
     if (methods.indexOf(input.method) == -1 || !input.hasOwnProperty('method')) {
         throw new Error(`Invalid request header: Invalid Method`);
@@ -10,7 +10,7 @@ function requestValidator(input) {
         throw new Error(`Invalid request header: Invalid URI`);
     } else if (versions.indexOf(input.version) == -1 || !input.hasOwnProperty('version')) {
         throw new Error(`Invalid request header: Invalid Version`);
-    } else if (!msgRegex.exec(input.message) && input.message !== '' || !input.hasOwnProperty('message')) {
+    } else if (!msgRegex.exec(input.message) || !input.hasOwnProperty('message')) {
         throw new Error(`Invalid request header: Invalid Message`);
     }
     return input;
