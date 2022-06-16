1cce12c26ae093dda0a2518a658839f54ecdacd2
niki-369 <nindzhiev@gmail.com>
2022-06-16 18:43:51 +0300

    commit

--- 03.JS Advanced/16.Classes and Attributes - Exercise/13.String Builder/stringBuilder.test.js
+++ 03.JS Advanced/16.Classes and Attributes - Exercise/13.String Builder/stringBuilder.test.js
@@ -24,38 +24,36 @@ describe("Tests:", () => {
     describe("Test functionality", () => {
         
         describe('test append', () => {
+            beforeEach(() => {
+                str = new StringBuilder('hello');
+            });
+
             it('with empty string', () => {
-                let str = new StringBuilder('hello');
                 str.append('');
                 expect(str.toString()).to.eq('hello');
             });
 
             it('with interval', () => {
-                let str = new StringBuilder('hello');
                 str.append(' ');
                 expect(str.toString()).to.eq('hello ');
             });
 
             it('with 1 letter', () => {
-                let str = new StringBuilder('hello');
                 str.append('a');
                 expect(str.toString()).to.eq('helloa');
             });
 
             it('with 3 letters', () => {
-                let str = new StringBuilder('hello');
                 str.append('abc');
                 expect(str.toString()).to.eq('helloabc');
             });
 
             it('with 1 number', () => {
-                let str = new StringBuilder('hello');
                 str.append('1');
                 expect(str.toString()).to.eq('hello1');
             });
 
             it('with 3 numbers', () => {
-                let str = new StringBuilder('hello');
                 str.append('123');
                 expect(str.toString()).to.eq('hello123');
             });
@@ -69,38 +67,36 @@ describe("Tests:", () => {
         });
 
         describe('test prepend', () => {
+            beforeEach(() => {
+                str = new StringBuilder('hello');
+            });
+
             it('with empty string', () => {
-                let str = new StringBuilder('hello');
                 str.prepend('');
                 expect(str.toString()).to.eq('hello');
             });
 
             it('with interval', () => {
-                let str = new StringBuilder('hello');
                 str.prepend(' ');
                 expect(str.toString()).to.eq(' hello');
             });
 
             it('with 1 letter', () => {
-                let str = new StringBuilder('hello');
                 str.prepend('a');
                 expect(str.toString()).to.eq('ahello');
             });
 
             it('with 3 letters', () => {
-                let str = new StringBuilder('hello');
                 str.prepend('abc');
                 expect(str.toString()).to.eq('abchello');
             });
 
             it('with 1 number', () => {
-                let str = new StringBuilder('hello');
                 str.prepend('1');
                 expect(str.toString()).to.eq('1hello');
             });
 
             it('with 3 numbers', () => {
-                let str = new StringBuilder('hello');
                 str.prepend('123');
                 expect(str.toString()).to.eq('123hello');
             });
@@ -114,38 +110,36 @@ describe("Tests:", () => {
         });
 
         describe('test insertAt', () => {
+            beforeEach(() => {
+                str = new StringBuilder('hello');
+            });
+
             it('at start', () => {
-                let str = new StringBuilder('hello');
                 str.insertAt('ab', 0);
                 expect(str.toString()).to.eq('abhello'); 
             });
             
             it('at middle', () => {
-                let str = new StringBuilder('hello');
                 str.insertAt('ab', 3);
                 expect(str.toString()).to.eq('helablo');
             });
 
             it('interval at middle', () => {
-                let str = new StringBuilder('hello');
                 str.insertAt(' ', 3);
                 expect(str.toString()).to.eq('hel lo');
             });
             
             it('at end', () => {
-                let str = new StringBuilder('hello');
                 str.insertAt('ab', 5);
                 expect(str.toString()).to.eq('helloab');
             });
             
             it('insert empty string', () => {
-                let str = new StringBuilder('hello');
                 str.insertAt('', 5);
                 expect(str.toString()).to.eq('hello');
             });
             
             it('invalid insert', () => {
-                let str = new StringBuilder('hello');
                 expect(() => str.insertAt(undefined, 0)).to.throw('Argument must be a string');
                 expect(() => str.insertAt(1, 0)).to.throw('Argument must be a string');
                 expect(() => str.insertAt(undefined)).to.throw('Argument must be a string');
@@ -154,69 +148,62 @@ describe("Tests:", () => {
         });
 
         describe('test remove', () => {
+            beforeEach(() => {
+                str = new StringBuilder('hello');
+            });
+            
             it('delete from start', () => {
-                let str = new StringBuilder('hello');
                 str.remove(0, 1);
                 expect(str.toString()).to.eq('ello');
             });
 
             it('delete from inside to end', () => {
-                let str = new StringBuilder('hello');
                 str.remove(2, 3);
                 expect(str.toString()).to.eq('he');
             });
 
             it('delete middle', () => {
-                let str = new StringBuilder('hello');
                 str.remove(1, 3);
                 expect(str.toString()).to.eq('ho');
             });
 
             it('delete middle (longer length', () => {
-                let str = new StringBuilder('hello');
                 str.remove(1, 10);
                 expect(str.toString()).to.eq('h');
             });
 
             it('delete last letter', () => {
-                let str = new StringBuilder('hello');
                 str.remove(4, 1);
                 expect(str.toString()).to.eq('hell');
             });
 
             it('delete whole string', () => {
-                let str = new StringBuilder('hello');
                 str.remove(0, 5);
                 expect(str.toString()).to.eq('');
             });
 
             it('delete whole string (longer length)', () => {
-                let str = new StringBuilder('hello');
                 str.remove(0, 10);
                 expect(str.toString()).to.eq('');
             });
 
             it('delete nothing from start', () => {
-                let str = new StringBuilder('hello');
                 str.remove(0, 0);
                 expect(str.toString()).to.eq('hello');
             });
 
 
             it('delete nothing from inside', () => {
-                let str = new StringBuilder('hello');
                 str.remove(2, 0);
                 expect(str.toString()).to.eq('hello');
             });
 
             it('delete nothing from end', () => {
-                let str = new StringBuilder('hello');
                 str.remove(5, 0);
                 expect(str.toString()).to.eq('hello');
             });
 
             it('delete nothing from end (invalid length)', () => {
-                let str = new StringBuilder('hello');
                 str.remove(5, 1);
                 expect(str.toString()).to.eq('hello');
             });
